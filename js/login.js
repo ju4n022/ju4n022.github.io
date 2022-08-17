df733f6f729a89f03bc108160b2d07c0f701cd7c
ju4n022 <juanmallopis22@gmail.com>
2022-08-16 22:59:42 -0300

    actualice login.js

--- js/login.js
+++ js/login.js
@@ -1,17 +1,8 @@
-function validar(){
+function validar() {
     //capturar valores de los campos//
-
-    noVacio = document.getElementById("floatingInput").value;
-    numero = document.getElementById("floatingPassword").value;
-    console.log(NoVacio);
-    console.log(numero);
-
-//validacion Novacio
-
-if(noVacio.length==0 || numero.lenght==0){
-    alert("ingrese algun valor")
-}
-
-validar()
-
-}
+    let email = document.getElementById("floatingInput").value;
+    let password = document.getElementById("floatingPassword").value;
+    //validacion
+    if (email == "" || password == "") {
+        alert("ingrese algun valor")
+    }}
