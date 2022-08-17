576834d7d78b4f3cc0f9e13940a0df3857b33bcd
ju4n022 <juanmallopis22@gmail.com>
2022-08-16 22:37:36 -0300

    formato

--- js/login.js
+++ js/login.js
@@ -1,17 +1,9 @@
 function validar() {
     //capturar valores de los campos//
-
     let email = document.getElementById("floatingInput").value;
     let password = document.getElementById("floatingPassword").value;
-    console.log(email);
-    console.log(password);
-
-    //validacion Novacio
-
-    if (email =="" || password =="") {
+    //validacion
+    if (email == "" || password == "") {
         alert("ingrese algun valor")
     }
-
-    
-
 }
