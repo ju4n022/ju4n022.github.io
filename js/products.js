7672876bd0ecc8468436916aece1723a7c363c66
ju4n022 <juanmallopis22@gmail.com>
2022-09-02 14:51:54 -0300

    cambio importante
    Cambié la forma de obtener la info de los json debido a que con await fetch no encontré solucion. Comento en el código los cambios //...//

--- js/products.js
+++ js/products.js
@@ -1,89 +1,103 @@
-const lista_autos = await fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
-.then(response => response.json());
-const lista_juguetes = await fetch("https://japceibal.github.io/emercado-api/cats_products/102.json")
-.then(response => response.json());
-const lista_muebles = await fetch("https://japceibal.github.io/emercado-api/cats_products/103.json")
-.then(response => response.json());
-const lista_herramientas = await fetch("https://japceibal.github.io/emercado-api/cats_products/104.json")
-.then(response => response.json());
-const lista_computadoras = await fetch("https://japceibal.github.io/emercado-api/cats_products/105.json")
-.then(response => response.json());
-const lista_vestimenta = await fetch("https://japceibal.github.io/emercado-api/cats_products/106.json")
-.then(response => response.json());
-const lista_electrodomesticos = await fetch("https://japceibal.github.io/emercado-api/cats_products/107.json")
-.then(response => response.json());
-const lista_deporte = await fetch("https://japceibal.github.io/emercado-api/cats_products/108.json")
-.then(response => response.json());
-const lista_celulares = await fetch("https://japceibal.github.io/emercado-api/cats_products/109.json")
-.then(response => response.json());
+const url_lista_autos = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const url_lista_juguetes = "https://japceibal.github.io/emercado-api/cats_products/102.json"
+const url_lista_muebles = "https://japceibal.github.io/emercado-api/cats_products/103.json"
+const url_lista_herramientas = "https://japceibal.github.io/emercado-api/cats_products/104.json"
+const url_lista_computadoras = "https://japceibal.github.io/emercado-api/cats_products/105.json"
+const url_lista_vestimenta = "https://japceibal.github.io/emercado-api/cats_products/106.json"
+const url_lista_electrodomesticos = "https://japceibal.github.io/emercado-api/cats_products/107.json"
+const url_lista_deporte = "https://japceibal.github.io/emercado-api/cats_products/108.json"
+const url_lista_celulares = "https://japceibal.github.io/emercado-api/cats_products/109.json"
+
+//defino las listas mediante const = url que luego invocaré, la maryoria están vacios//
 
 
-//mayoria vacios//
+let minPrice = undefined;
+let maxPrice = undefined;
+const ORDER_BY_PRICE = "Price.";
+let currentProductsArray = [];
+
+//defino constantes y variables que necesitaré para los filtros
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    let categorias = localStorage.getItem("catID");
+    let url = "";
+
+    //carga los datos "DOM" del url que le indico, segun su catID
+
+    if (categorias == 101) {
+        url = url_lista_autos;
+    }
+    if (categorias == 102) {
+        url = url_lista_juguetes;
+    }
+    if (categorias == 103) {
+        url = url_lista_muebless;
+    }
+    if (categorias == 104) {
+        url = url_lista_herramientas;
+    }
+    if (categorias == 105) {
+        url = url_lista_computadoras;
+    }
+    if (categorias == 106) {
+        url = url_lista_vestimenta;
+    }
+    if (categorias == 107) {
+        url = url_lista_electrodomesticos;
+    }
+    if (categorias == 108) {
+        url = url_lista_deporte;
+    }
+    if (categorias == 109) {
+        url = url_lista_celulares;
+    }
+
+
+    getJSONData(url).then(function (resultObj) {
+        if (resultObj.status === 'ok') {
+            currentProductsArray = resultObj.data
+            FilterAndSortPage();
+        }
+    });
+});
 
 
 
 
 
-    
-//console.log(lista_autos.products[1].name)//
+
+
 const contenedor = document.getElementById("contenedor");
 
-function pagina(lista_de_autos) {
+function FilterAndSortPage() {
 
-    for (let i = 0; i < lista_de_autos.products.length; i++) {
-        contenedor.innerHTML += `<div class="row">
+    var min = minPrice;
+    var max = maxPrice;
+
+    for (let i = 0; i < currentProductsArray.products.length; i++) {
+        var product = currentProductsArray.products[i];
+
+        if (((min == undefined) || (min != undefined && parseInt(product.cost) >= min)) &&
+            ((max == undefined) || (max != undefined && parseInt(product.cost) <= max))) {
+
+            contenedor.innerHTML += `<div class="row">
         <div class="col-3">
-            <img src="${lista_de_autos.products[i].image}" alt="NO SE CARGA LA IMAGEN" class="img-thumbnail">
+            <img src= ${product.image} alt='NO SE CARGA LA IMAGEN' class='img-thumbnail'>
         </div>
         <div class="col">
             <div class="d-flex w-100 justify-content-between">
-                <h4 class="mb-1">${lista_de_autos.products[i].name} - ${lista_de_autos.products[i].currency} ${lista_de_autos.products[i].cost}  </h4>
-                <small class="text-muted">${lista_de_autos.products[i].soldCount} artículos</small>
+                <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}  </h4>
+                <small class="text-muted">${product.soldCount} artículos</small>
             </div>
-            <p class="mb-1">${lista_de_autos.products[i].description}</p>
+            <p class="mb-1">${product.description}</p>
         </div>
     </div> `
-        
+        }
+
 
     }
 }
-
-let productos;
-let categorias = localStorage.getItem("catID");
-
-//armo la variable categorias que va obteniendo segun cat ID, luego la variable productos va a ir cambiano segun este valor de categorias//
-
-if(categorias == 101){ 
-    productos = lista_autos;
-
-}
-else if (categorias == 102){
-    productos = lista_juguetes;
-}
-
-else if (categorias == 103){
-    productos = lista_muebles;
-}
-else if (categorias == 104){
-    productos = lista_herramientas;
-}
-else if (categorias == 105){
-    productos = lista_computadoras;
-}
-else if (categorias == 106){
-    productos = lista_vestimenta;
-}
-else if (categorias == 107){
-    productos = lista_electrodomesticos;
-}
-else if (categorias == 108){
-    productos = lista_deporte;
-}
-else if (categorias == 109){
-    productos = lista_celulares;
-}
-
-pagina(productos)
 
 
 
