d2034c5e6c3b53a03f1f72e73f4fca914dd6c237
ju4n022 <juanmallopis22@gmail.com>
2022-09-13 20:00:01 -0300

    Estrellas e imagenes
    Cambié el css para mostrar estrellas.
    cambios con commits para mostrar imagenes del producto(forearch)

--- js/product-info.js
+++ js/product-info.js
@@ -31,65 +31,79 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 });
 
-
-function LoadComments(){
+function LoadComments() {
   let productinfo = localStorage.getItem("catIDinfo"); //obtengo por id mediante storage
 
   let url_comments = comments + productinfo + ".json";
-  
+
   getJSONData(url_comments).then(function (resultObj) {
-  
+
     if (resultObj.status === 'ok') {
-  
+
       currentCommentsInfo = resultObj.data //currentCommentsInfo es la data del result.Obj
       MostrarComments(); //llamo a la funcion mostrar info para que añada esta informacion obtenida a la página mediante innerHTML
     }
   });
 
 }
-
-
-
-function MostrarInfo() {
+function MostrarInfo() { //funcion que llamaré arriba para mostrar la info del producto que corresponda, accedo al elemento mediante ${lista.dato} y los pego mediante innerHTML
 
   prodinfo.innerHTML = "";
-
   prodinfo.innerHTML = `
-    
-    
         <div> <h1>${currentProductInfo.name}</h1>
-
             <h3> Precio: ${currentProductInfo.currency} ${currentProductInfo.cost}</h3>
+            <h3> Hemos vendido: ${currentProductInfo.soldCount} artículos </h3>
+        <b>"Descripcion"</b>${currentProductInfo.description}</div> 
+        </br>
+        `
+        currentProductInfo.images.forEach(element => { //recorro las imagenes de los productos con foreach y las llamo "element", dicho element será la src de la imagen de cada elemento
+          prodinfo.innerHTML += `
+          <div class="gallery"> 
+          <img src="${element}" width="600" height="400">
+          </div>`
 
-            <h3> Hemos vendido: ${currentProductInfo.soldCount} artículos</h3>
-            
-        
-        <b>"Descripcion"</b>${currentProductInfo.description}</div>
+      //uso la clase "gallery" y en ella agrego un IMG con los sources de cada imagen que se las traigo con ${element}, luego defino ancho y largo
 
-        
+  });   
 
-
-` //FALTAN IMAGENES
 }
 
-
-
-function MostrarComments() {  //funcion que recorre los comentarios con foreach
+function MostrarComments() { //armo funcion para mostrar comentarios de cada producto
 
   commentsinfo.innerHTML = "";
 
-  currentCommentsInfo.forEach(comentario => {
+  currentCommentsInfo.forEach(comentario => { //recorro la lista de comentarios y llamo comentario a cada uno de ellos, luego armo un for para el score de las estrellas
+
+    let score = ''; //defino variable que luego invocaré para agregar a un span que tendrá estrellas checked y no checked
+
+    for (let i = 1; i <= 5; i++) {
+      if(i<=comentario.score){ // la condicion: si ese i es menor o igual al score de ese comentario, entonces agrego una estrella pintada por cada "score" que tenga
+        score += '<span class="fa fa-star checked"></span>';
+
+      }
+      else { //si no pasa lo de arriba agrego una negra (sin  pintar)
+        score += '<span class="fa fa-star"></span>';
+      }
+      
+    }
+
+
+
+
+    commentsinfo.innerHTML += `
     
-    commentsinfo.innerHTML += `<div class="card">
+    </br>
+    
+    <div class="card">
   
     <div class="col">
         <div>
-            <h4 class="mb-1">${comentario.user} - ${comentario.dateTime} ${comentario.score}  </h4>
+        <h4> ${comentario.user} - FECHA Y HORA: ${comentario.dateTime} <span> - ${score} </span></h4>
+
+           
             
         <p class="mb-1">${comentario.description}</p>
     </div>
-  </div> ` //cambiar forma de mostrar y diseño
-  
-
+  </div> ` // score esta condicionado en el for de arriba
   });
 }
